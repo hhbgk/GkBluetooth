81c9cbec1539b1886817d55ff39af99269691f83
hhbgk <hhbjsj@163.com>
2020-05-09 19:32:02 +0800

    Add ble support.

--- app/src/main/java/com/gk/app/bluetooth/ble/BleClientFragment.java
+++ app/src/main/java/com/gk/app/bluetooth/ble/BleClientFragment.java
@@ -1,19 +1,34 @@
 package com.gk.app.bluetooth.ble;
 
+import android.bluetooth.BluetoothGatt;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gk.app.bluetooth.R;
+import com.gk.app.bluetooth.adapter.DeviceAdapter;
+import com.gk.app.bluetooth.base.BaseFragment;
+import com.gk.app.bluetooth.listener.OnItemClickListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BleClientFragment extends Fragment {
+public class BleClientFragment extends BaseFragment {
+    private RecyclerView rv;
+    private EditText mWriteET;
+    private TextView mTips;
+    private DeviceAdapter mBleDevAdapter;
+    private BluetoothGatt mBluetoothGatt;
+    private boolean isConnected = false;
+    private final DeviceAdapter mAdapter = new DeviceAdapter();
 
     public BleClientFragment() {
         // Required empty public constructor
@@ -21,9 +36,29 @@ public class BleClientFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ble_client, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_ble_client, container, false);
+        rv = v.findViewById(R.id.rv_ble);
+        return v;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mAdapter.setOnItemClickListener(onItemClickListener);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(mAdapter);
+    }
+
+    private final OnItemClickListener onItemClickListener = new OnItemClickListener() {
+        @Override
+        public void onItemClick(View view, Object o, int position) {
+
+        }
+
+        @Override
+        public void onItemLongClick(View view, Object o, int position) {
+
+        }
+    };
 }
